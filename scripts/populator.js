feb7353b27f8d40ce13afd91e77f4945ed53112d
Favour Nerrise <favour-nerrise@users.noreply.github.com>
2018-03-23 23:16:36 -0400

    Updated ES6 syntax and minor debug fixes

--- scripts/populator.js
+++ scripts/populator.js
@@ -1,9 +1,8 @@
+const neighbourhoods = require("./neighbourhoods.json");
 
-var neighbourhoods = require("./neighbourhoods.json");
-
-var mongoose = require("mongoose");
-var promisify = require("../helpers/promisify");
-var bluebird = require("bluebird")
+const mongoose = require("mongoose");
+const promisify = require("promisify");
+const bluebird = require("bluebird")
 
 mongoose.Promise = bluebird;
 
@@ -14,13 +13,13 @@ mongoose.connection.once('open', function(){
 
   require("../models")();
 
-  var Hood = mongoose.model("Hood");
+  let Hood = mongoose.model("Hood");
 
 
-  var promises = neighbourhoods.features.map(function(neighbourhood){
-    var hood = new Hood();
-    var name = neighbourhood.attributes.AREA_NAME;
-    var parts = name.match(/^(.+)\s\(([0-9]+)\)$/);
+  let promises = neighbourhoods.features.map(function(neighbourhood){
+    let hood = new Hood();
+    let name = neighbourhood.attributes.AREA_NAME;
+    let parts = name.match(/^(.+)\s\(([0-9]+)\)$/);
 
     hood.nid = parts[2];
     hood.title = parts[1];
